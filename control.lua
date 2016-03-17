fdb9594ea7f3b75d76d4f89de170de799d8ed02d
David Sanders <david.wm.sanders@gmail.com>
2016-03-17 02:39:41 +0000

    Add formatted time to timelapse image names

--- control.lua
+++ control.lua
@@ -1,5 +1,13 @@
 require "defines"
 
+-- Derived from util.formattime
+function ftime(ticks)
+  local seconds = ticks / 60
+  local min = math.floor(seconds / 60)
+  local sec = math.floor(seconds - 60 * min)
+  return string.format("%d_%02d", min, sec)
+end
+
 script.on_event(defines.events.on_tick, function(event)
   if (game.tick % 1800 == 0 and game.tick > 0) then
     local player = game.player
@@ -8,6 +16,6 @@ script.on_event(defines.events.on_tick, function(event)
     local z = 0.29
     local seed = player.surface.map_gen_settings.seed
     game.player.print("x")
-    game.take_screenshot{position=spawnpos, resolution=res, zoom=z, path="timelapse/"..seed.."_"..game.tick..".png", show_gui=false, show_entity_info=false}
+    game.take_screenshot{position=spawnpos, resolution=res, zoom=z, path="timelapse/"..seed.."_"..game.tick.."_"..ftime(game.tick)..".png", show_gui=false, show_entity_info=false}
     end
 end)
